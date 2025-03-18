795e3bba0a6e52e79abdd20fe02608cf053939bc
honjow <honjow311@gmail.com>
2025-03-18 16:57:28 +0800

    test copy aurs

--- pre-build-image.sh
+++ pre-build-image.sh
@@ -11,3 +11,32 @@ if [ -z "${BUILD_BRANCH}" ]; then
 fi
 
 cp -rav branch/${BUILD_BRANCH}/rootfs/* rootfs/
+cp -f branch/${BUILD_BRANCH}/sub-manifest .
+
+mv aur-pkgs aur-pkgs-ori
+mv pkgs pkgs-ori
+
+source manifest
+source sub-manifest
+
+# branch aur packages
+for package in ${SUB_AUR_PACKAGES}; do
+  echo "copying ${package}"
+  cp -rv aur-pkgs-ori/[${package}]-*.pkg.tar* aur-pkgs/ || true
+done
+
+# public aur packages
+for package in ${AUR_PACKAGES}; do
+  echo "copying ${package}"
+  cp -rv aur-pkgs-ori/[${package}]-*.pkg.tar* aur-pkgs/ || true
+done
+
+# branch local packages
+for package in ${SUB_PACKAGES}; do
+  echo "copying ${package}"
+  cp -rv pkgs-ori/[${package}]-*.pkg.tar* pkgs/ || true
+done
+
+ls -l aur-pkgs
+ls -l pkgs
+
