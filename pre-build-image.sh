ee00ca58277f01a729287b889d9d1120148df0ad
honjow <honjow311@gmail.com>
2025-03-18 17:42:27 +0800

    fix match

--- pre-build-image.sh
+++ pre-build-image.sh
@@ -16,25 +16,28 @@ cp -f branch/${BUILD_BRANCH}/sub-manifest .
 mv aur-pkgs aur-pkgs-ori
 mv pkgs pkgs-ori
 
+mkdir -p aur-pkgs
+mkdir -p pkgs
+
 source ./manifest
 source ./sub-manifest
 
 # branch aur packages
 for package in ${SUB_AUR_PACKAGES}; do
   echo "copying ${package}"
-  cp -rv aur-pkgs-ori/[${package}]-*.pkg.tar* aur-pkgs/ || true
+  cp -rv "aur-pkgs-ori/[${package}]-"*.pkg.tar* aur-pkgs/ || true
 done
 
 # public aur packages
 for package in ${AUR_PACKAGES}; do
   echo "copying ${package}"
-  cp -rv aur-pkgs-ori/[${package}]-*.pkg.tar* aur-pkgs/ || true
+  cp -rv "aur-pkgs-ori/[${package}]-"*.pkg.tar* aur-pkgs/ || true
 done
 
 # branch local packages
 for package in ${SUB_PACKAGES}; do
   echo "copying ${package}"
-  cp -rv pkgs-ori/[${package}]-*.pkg.tar* pkgs/ || true
+  cp -rv "pkgs-ori/[${package}]-"*.pkg.tar* pkgs/ || true
 done
 
 ls -l aur-pkgs
