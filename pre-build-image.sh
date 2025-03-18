136a544ab53c12e6c62d792e5dc8db8e2a990060
honjow <honjow311@gmail.com>
2025-03-18 18:00:51 +0800

    Add SUB_LOCAL_PACKAGES

--- pre-build-image.sh
+++ pre-build-image.sh
@@ -13,33 +13,37 @@ fi
 cp -rav branch/${BUILD_BRANCH}/rootfs/* rootfs/
 cp -f branch/${BUILD_BRANCH}/sub-manifest .
 
+source ./manifest
+source ./sub-manifest
+
 mv aur-pkgs aur-pkgs-ori
 mv pkgs pkgs-ori
 
 mkdir -p aur-pkgs
 mkdir -p pkgs
 
-source ./manifest
-source ./sub-manifest
-
 # branch aur packages
 for package in ${SUB_AUR_PACKAGES}; do
-  echo "copying ${package}"
+  # echo "copying ${package}"
   cp -rv "aur-pkgs-ori/[${package}]-"*.pkg.tar* aur-pkgs/ || true
 done
 
 # public aur packages
 for package in ${AUR_PACKAGES}; do
-  echo "copying ${package}"
+  # echo "copying ${package}"
   cp -rv "aur-pkgs-ori/[${package}]-"*.pkg.tar* aur-pkgs/ || true
 done
 
 # branch local packages
-for package in ${SUB_PACKAGES}; do
-  echo "copying ${package}"
+for package in ${SUB_LOCAL_PACKAGES}; do
+  # echo "copying ${package}"
   cp -rv "pkgs-ori/[${package}]-"*.pkg.tar* pkgs/ || true
 done
 
-ls -l aur-pkgs
-ls -l pkgs
+# ls -l ./aur-pkgs
+# ls -l ./pkgs
 
+rm -rf aur-pkgs-ori
+rm -rf pkgs-ori
+
+tree
