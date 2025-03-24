31ab598f975fb1cbd1d25229d9a23708de69470f
honjow <honjow311@gamil.com>
2025-03-25 02:56:59 +0800

    kde: add fcitx input method support to gtkrc and settings.ini

--- merge-aurs.sh
+++ merge-aurs.sh
@@ -12,11 +12,11 @@ for sub_manifest in "${sub_manifest_paths[@]}"; do
 done
 
 # remove duplicates
-AUR_PACKAGES=$(echo "${AUR_PACKAGES}" | tr ' ' '\n' | sort -u | tr '\n' ' ')
+AUR_PACKAGES=$(echo ${AUR_PACKAGES} | tr ' ' '\n' | sort -u | tr '\n' ' ')
 
-echo "${AUR_PACKAGES}" | tr -s ' \n' ' ' | jq -R -s -c 'split(" ") | map(select(length > 0))'
-json_list=$(echo "${AUR_PACKAGES}" | tr -s ' \n' ' ' | jq -R -s -c 'split(" ") | map(select(length > 0))')
-echo "${json_list}"
+echo ${AUR_PACKAGES} | tr -s ' \n' ' ' | jq -R -s -c 'split(" ") | map(select(length > 0))'
+json_list=$(echo ${AUR_PACKAGES} | tr -s ' \n' ' ' | jq -R -s -c 'split(" ") | map(select(length > 0))')
+echo ${json_list}
 
 if [ -f "${GITHUB_OUTPUT}" ]; then
   echo "matrix=${json_list}" >> "${GITHUB_OUTPUT}"
