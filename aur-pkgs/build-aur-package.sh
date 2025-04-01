e205f7373242a20154d4fa6010bb7589ec68ee7f
honjow <honjow311@gmail.com>
2025-04-02 03:10:04 +0800

    build-scripts: optimize pikaur commands with MAKEFLAGS and update exit status

--- aur-pkgs/build-aur-package.sh
+++ aur-pkgs/build-aur-package.sh
@@ -11,7 +11,7 @@ sudo chown build:build /workdir/aur-pkgs
 
 git clone --depth=1 https://aur.archlinux.org/${1}.git /temp/package
 
-PIKAUR_CMD="PKGDEST=/workdir/aur-pkgs pikaur --noconfirm --build-gpgdir /etc/pacman.d/gnupg -S -P /temp/package/PKGBUILD"
+PIKAUR_CMD="PKGDEST=/workdir/aur-pkgs MAKEFLAGS=-j$(nproc) pikaur --noconfirm --build-gpgdir /etc/pacman.d/gnupg -S -P /temp/package/PKGBUILD"
 PIKAUR_RUN=(bash -c "${PIKAUR_CMD}")
 
 # 重试次数
@@ -38,5 +38,5 @@ set -e
 # 如果重试3次后仍然失败，则退出
 if [ ${RETRY_COUNT} -eq ${MAX_RETRIES} ]; then
     echo ">>>>>> Build failed after ${MAX_RETRIES} attempts. Stopping..."
-    exit -1
+    exit 1
 fi
